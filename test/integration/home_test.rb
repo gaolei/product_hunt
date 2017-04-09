64145f90e31fb47389b3e7d4af4d1cb17df3dc61
Eric-Guo <guochunzhong@bayekeji.com>
2017-04-09 17:33:01 +0800

    using chrome

--- test/integration/home_test.rb
+++ test/integration/home_test.rb
@@ -4,7 +4,7 @@ class HomeTest < ActionDispatch::IntegrationTest
   include Capybara::DSL
   test 'loads correctly' do
     visit '/'
-    assert_equal 200, page.status_code
+    assert_title 'ProductHunt'
     assert page.has_content?('Products')
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'capybara/minitest'
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.javascript_driver = :chrome
+
+Capybara.configure do |config|
+  config.default_max_wait_time = 10 # seconds
+  config.default_driver        = :selenium
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
